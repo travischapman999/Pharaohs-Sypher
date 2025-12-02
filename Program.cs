bd46367bd93ea5d5f8213a313ac6708ad1897fa4
unknown <travis.chapman@ENGINEERING.SNOW>
2025-12-02 15:42:39 -0700

    Start of Lab

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+System.Console.WriteLine("automated testing ...");
+System.Diagnostics.Debug.Assert(ShiftedCharacter('a') == 'b');
+System.Console.WriteLine("tests passed.");
+
+char ShiftedCharacter(char originalCharacter)
+{
+    return (char)(originalCharacter + 1 > 'z' ? originalCharacter - 25 : originalCharacter + 1);
+}
+System.Diagnostics.Debug.Assert(ShiftedCharacter('a') == 'b');
+System.Console.WriteLine();
+System.Diagnostics.Debug.Assert(AsChar('a' + 0) == 'a');
+System.Diagnostics.Debug.Assert(AsChar('a' + 1) == 'b');
+System.Diagnostics.Debug.Assert(AsChar('a' + 3) == 'd');
+
+char AsChar(int asciiValue)
+{
+    return (char)asciiValue;
+}
+
+while (true)
+{
+    char originalCharacter = Console.ReadKey(true).KeyChar;
+    if (originalCharacter == '`') break;
+    Console.Write(ShiftedCharacter((char)originalCharacter));
+
+}
